d275a5da2e2a6aad4bd85016341273c599d6ac7a
André Oliveira <andre.walkermo@gmail.com>
2020-03-06 18:26:41 -0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=equipesi_nell',
-    'username' => 'phpmyadmin',
-    'password' => 'root',
+    'dsn' => 'mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_e255a5ab1ef3ca4',
+    'username' => 'bd11a02899de47',
+    'password' => '0a7dc8e6',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
