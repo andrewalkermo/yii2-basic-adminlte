fe93b613812a32c7323f80d9174b0694cf13a691
André Walker <andre.walkermo@gmail.com>
2020-07-03 16:40:20 -0300

    mid gii with relations

---
+++ models/Usuarios.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $auth_key
+ * @property string $password_hash
+ * @property string|null $confirmation_token
+ * @property int $status
+ * @property int|null $superadmin
+ * @property int $created_at
+ * @property int $updated_at
+ * @property string|null $registration_ip
+ * @property string|null $bind_to_ip
+ * @property string|null $email
+ * @property int $email_confirmed
+ *
+ * @property AuthAssignment[] $authAssignments
+ * @property AuthItem[] $itemNames
+ * @property UserVisitLog[] $userVisitLogs
+ */
+class Usuarios extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'auth_key', 'password_hash', 'created_at', 'updated_at'], 'required'],
+            [['status', 'superadmin', 'created_at', 'updated_at', 'email_confirmed'], 'integer'],
+            [['username', 'password_hash', 'confirmation_token', 'bind_to_ip'], 'string', 'max' => 255],
+            [['auth_key'], 'string', 'max' => 32],
+            [['registration_ip'], 'string', 'max' => 15],
+            [['email'], 'string', 'max' => 128],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'username' => Yii::t('app', 'Username'),
+            'auth_key' => Yii::t('app', 'Auth Key'),
+            'password_hash' => Yii::t('app', 'Password Hash'),
+            'confirmation_token' => Yii::t('app', 'Confirmation Token'),
+            'status' => Yii::t('app', 'Status'),
+            'superadmin' => Yii::t('app', 'Superadmin'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+            'registration_ip' => Yii::t('app', 'Registration Ip'),
+            'bind_to_ip' => Yii::t('app', 'Bind To Ip'),
+            'email' => Yii::t('app', 'Email'),
+            'email_confirmed' => Yii::t('app', 'Email Confirmed'),
+        ];
+    }
+
+    /**
+     * Gets query for [[AuthAssignments]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthAssignments()
+    {
+        return $this->hasMany(AuthAssignment::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[ItemNames]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItemNames()
+    {
+        return $this->hasMany(AuthItem::className(), ['name' => 'item_name'])->viaTable('auth_assignment', ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[UserVisitLogs]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserVisitLogs()
+    {
+        return $this->hasMany(UserVisitLog::className(), ['user_id' => 'id']);
+    }
+}
---
+++ views/controller/create.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Usuarios */
+
+$this->title = 'Criar Usuarios';
+$this->params['breadcrumbs'][] = ['label' => 'Usuarios', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="usuarios-create box center-block box-primary" style="margin-top: 20px; padding: 20px">
+    <div class="box-header with-border">
+        <h1 style="text-align: center"><?= Html::encode($this->title) ?></h1>
+    </div>
+    <div class="box-body">
+        <?= $this->render('_form', [
+            'model' => $model,
+        ]) ?>
+    </div>
+</div>
